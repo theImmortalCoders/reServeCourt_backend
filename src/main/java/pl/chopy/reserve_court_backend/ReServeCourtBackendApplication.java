bdfe1f2b2106667d6257c2f3f84fef3bba179557
marcinbator <marcinbator.ofc@gmail.com>
2024-03-04 18:20:49 +0100

    initial commit

---
+++ src/main/java/pl/chopy/reserve_court_backend/ReServeCourtBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.chopy.reserve_court_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServeCourtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReServeCourtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/chopy/reserve_court_backend/ReServeCourtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chopy.reserve_court_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReServeCourtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
