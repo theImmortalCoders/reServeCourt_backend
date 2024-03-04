386aba46cdf06db839216398efd5c2471cb765d7
marcinbator <marcinbator.ofc@gmail.com>
2024-03-04 18:20:49 +0100

    initial commit

---
+++ src/test/java/pl/chopy/tennis_king_backend/TennisKingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chopy.tennis_king_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisKingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
