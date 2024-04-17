9193ec2e4e51a7095ab55459ec76959cca964ae8
Marcin Bator <marcinbator.ofc@gmail.com>
2024-04-17 21:03:27 +0200

    feat: #20 management notification sending and retrieving system

--- src/main/java/pl/chopy/reserve_court_backend/infrastructure/notification/NotificationService.java
+++ src/main/java/pl/chopy/reserve_court_backend/infrastructure/notification/NotificationService.java
@@ -3,12 +3,14 @@ package pl.chopy.reserve_court_backend.infrastructure.notification;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import pl.chopy.reserve_court_backend.infrastructure.notification.dto.NotificationSingleRequest;
 
 @Service
 @AllArgsConstructor
 public class NotificationService {
-	@RabbitListener(queues = "myQueue")
-	public void listen(String in) {
-		System.out.println("Message read from myQueue : " + in);
+
+	@RabbitListener(queues = "managementQueue")
+	public void listen(NotificationSingleRequest notification) {
+		System.out.println("Message read from myQueue : " + notification.getMessage());
 	}
 }
