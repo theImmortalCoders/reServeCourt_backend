9a3f86f9e82d81290440af8e51af6b435f722b80
Marcin Bator <marcinbator.ofc@gmail.com>
2024-04-19 15:20:49 +0200

    feat: #20 basic ws massaging logic

--- src/main/java/pl/chopy/reserve_court_backend/config/WebSocketConfig.java
+++ src/main/java/pl/chopy/reserve_court_backend/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package pl.chopy.reserve_court_backend.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -9,10 +10,13 @@ import pl.chopy.reserve_court_backend.util.WebSocketHandler;
 
 @Configuration
 @EnableWebSocket
+@AllArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+	private final WebSocketHandler webSocketHandler;
+
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new WebSocketHandler(), "/ws")
+		registry.addHandler(webSocketHandler, "/ws")
 				.setAllowedOrigins("*");
 	}
 }
