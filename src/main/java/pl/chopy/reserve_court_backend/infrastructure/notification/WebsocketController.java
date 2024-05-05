2b69751a4482e52665f3ac911f310b4691ce2b93
Marcin Bator <marcinbator.ofc@gmail.com>
2024-05-05 22:16:15 +0200

    feat: #20 sending message on reservation basic workflow

--- src/main/java/pl/chopy/reserve_court_backend/infrastructure/notification/WebsocketController.java
+++ src/main/java/pl/chopy/reserve_court_backend/infrastructure/notification/WebsocketController.java
@@ -8,10 +8,16 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
 import pl.chopy.reserve_court_backend.infrastructure.notification.dto.NotificationMapper;
+import pl.chopy.reserve_court_backend.infrastructure.notification.dto.NotificationSingleRequest;
 import pl.chopy.reserve_court_backend.infrastructure.notification.dto.NotificationSingleResponse;
 import pl.chopy.reserve_court_backend.infrastructure.user.UserUtil;
+import pl.chopy.reserve_court_backend.model.entity.Notification;
 import pl.chopy.reserve_court_backend.model.entity.User;
 import pl.chopy.reserve_court_backend.model.entity.repository.NotificationRepository;
 
@@ -19,7 +25,7 @@ import java.security.Principal;
 import java.util.List;
 
 @AllArgsConstructor
-@Controller
+@RestController
 public class WebsocketController {
 	private final UserUtil userUtil;
 	private final NotificationRepository notificationRepository;
@@ -27,8 +33,29 @@ public class WebsocketController {
 	private final SimpMessagingTemplate simpMessagingTemplate;
 	private final ObjectMapper objectMapper;
 
+	@PostMapping("/api/socket/send")
+	public void send(@RequestBody NotificationSingleRequest notification) throws JsonProcessingException {
+		User user = userUtil.getUserById(notification.getReceiverId());
+
+		List<NotificationSingleResponse> notifications = notificationRepository
+				.findAllByReceiverIdAndRead(user.getId(), false)
+				.stream()
+				.map(notificationMapper::map)
+				.toList();
+
+		SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+		headerAccessor.setSessionId(user.getSessionId());
+		headerAccessor.setLeaveMutable(true);
+
+		simpMessagingTemplate.convertAndSend(
+				"/queue/reply",
+				objectMapper.writeValueAsString(notifications)
+		);
+
+	}
+
 	@MessageMapping("/broadcast")
-	public void broadcastMessage(@Header("simpSessionId") String sessionId, Principal principal) {
+	public void broadcastMessage(@Header("simpSessionId") String sessionId, Principal principal) throws JsonProcessingException {
 		if (principal == null) {
 			return;
 		}
@@ -45,15 +72,12 @@ public class WebsocketController {
 		headerAccessor.setSessionId(sessionId);
 		headerAccessor.setLeaveMutable(true);
 
-		try {
-			simpMessagingTemplate.convertAndSendToUser(
-					sessionId,
-					"/queue/reply",
-					objectMapper.writeValueAsString(notifications),
-					headerAccessor.getMessageHeaders());
-		} catch (JsonProcessingException ignored) {
-		}
-		System.out.println(notifications);
+		simpMessagingTemplate.convertAndSendToUser(
+				sessionId,
+				"/queue/reply",
+				objectMapper.writeValueAsString(notifications),
+				headerAccessor.getMessageHeaders());
+
 	}
 }
 
